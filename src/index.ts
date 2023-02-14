b01ba017e30d5b409bf263a0d7ad760f47f8d977
AnyWayDude <zpaulz61@gmail.com>
2023-02-15 00:13:29 +0100

    redner movie promise

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import { POPULAR_URL } from "./API";
+import { getFilms,displayPopularFilms } from "./fetching";
+
 export async function render(): Promise<void> {
     // TODO render your app here
+
+    const moviesDB = await getFilms(POPULAR_URL)
+     displayPopularFilms(moviesDB) 
 }
+
