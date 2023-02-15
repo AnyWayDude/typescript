3a2e3d942afa1410aac985f682e588fc159e5d62
AnyWayDude <zpaulz61@gmail.com>
2023-02-15 23:58:27 +0100

    small changes for favorite

--- src/movie-card.ts
+++ src/movie-card.ts
@@ -1,13 +1,17 @@
-export const creatMovieCard = (poster_img: string, overview: string, release_date: string ): string => {
+export const creatMovieCard = (poster_img: string, overview: string, release_date: string, id: string): string => {
+
+
+
     return `
     <div class="card shadow-sm">
     <img
                                     src='${poster_img}'
                                 />
                                 <svg
+                                    id=${id}
                                     xmlns="http://www.w3.org/2000/svg"
                                     stroke="red"
-                                    fill="red"
+                                    fill="none"
                                     width="50"
                                     height="50"
                                     class="bi bi-heart-fill position-absolute p-2"
@@ -18,6 +22,7 @@ export const creatMovieCard = (poster_img: string, overview: string, release_dat
                                         d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
                                     />
                                 </svg>
+                             
                                 <div class="card-body">
                                     <p class="card-text truncate">
                                         ${overview}
