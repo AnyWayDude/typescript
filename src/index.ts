11f28491c924731a96963a79949eb9f2449b84a3
AnyWayDude <zpaulz61@gmail.com>
2023-02-17 08:47:38 +0100

    add fav func

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { POPULAR_URL } from "./API";
-import { getFilms, displayPopularFilms, displaySearchFilms, displayCategoriesFilms, handleOnClick, loadMore } from "./fetching";
+import { getFilms, displayPopularFilms, displaySearchFilms, displayCategoriesFilms, handleOnClick, loadMore, displayFavFilms } from "./fetching";
 
 export async function render(): Promise<void> {
     // TODO render your app here
 
     // const moviesDB = await getFilms(POPULAR_URL)
     // displayPopularFilms(moviesDB)
-    getFilms(POPULAR_URL)
+    await getFilms(POPULAR_URL)
 
     const categoriesBtns = document.getElementById('button-wrapper')
     categoriesBtns?.addEventListener('click', displayCategoriesFilms)
@@ -14,11 +14,16 @@ export async function render(): Promise<void> {
     const searchFilms = document.getElementById('submit')
     searchFilms?.addEventListener('click', displaySearchFilms);
 
-    const favButton = document.getElementById('fav')
-    favButton?.addEventListener('click', handleOnClick);
+
 
     const loadMoreBtn = document.getElementById('load-more');
     loadMoreBtn?.addEventListener('click', loadMore);
+
+    const favButtons = document.querySelectorAll('#fav')
+
+    console.log(favButtons);
+
+    favButtons.forEach((btn) => btn.addEventListener('click', handleOnClick))
 
 
 
--- src/movie-card.ts
+++ src/movie-card.ts
@@ -7,22 +7,24 @@ export const creatMovieCard = (poster_img: string, overview: string, release_dat
     <img
                                     src='${poster_img}'
                                 />
+                                <div id='fav'  class="bi bi-heart-fill position-absolute p-2">
                                 <svg
                                     id=${id}
                                     xmlns="http://www.w3.org/2000/svg"
                                     stroke="red"
-                                    fill="none"
+                                    fill="transparent"
                                     width="50"
                                     height="50"
                                     class="bi bi-heart-fill position-absolute p-2"
                                     viewBox="0 -2 18 22"
                                 >
                                     <path
+                                    id=${id}
                                         fill-rule="evenodd"
                                         d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
                                     />
                                 </svg>
-                             
+                             </div>
                                 <div class="card-body">
                                     <p class="card-text truncate">
                                         ${overview}
